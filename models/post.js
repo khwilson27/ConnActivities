3f17c07dff48b756343f879b81512d2f5ab1db2c
Kevin Wilson <kevinhn27@gmail.com>
2017-07-06 15:01:47 -0700

    initial user and post models

---
+++ models/post.js
@@ -0,0 +1,42 @@
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    address: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      len: [1]
+    }
+  },
+    {
+      // We're saying that we want our Author to have Posts
+      classMethods: {
+        associate: function(models) {
+          // An Author (foreignKey) is required or a Post can't be made
+          Post.belongsTo(models.User, {
+            foreignKey: {
+              allowNull: false
+            }
+          });
+        }
+      }
+    }
+  );
+  
+  return Post;
+};
