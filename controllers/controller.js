25bcdbf39980510a3d008e9f0fbb80848dec53ce
Joseph Hernandez <josephhernandez@dhcp-v106-141.mobile.uci.edu>
2017-07-08 13:00:57 -0700

    merge

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,30 @@
 // Import the model (burger.js) to use its database functions.
+var path = require("path");
 var db = require("../models");
 
 module.exports = function (app) {
+   
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  // my-posts route loads cms.html
+  app.get("/my-posts", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/my-posts.html"));
+  });
+
+  // blog route loads blog.html
+  app.get("/sign-in", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/sign-in.html"));
+  });
+
+  // authors route loads author-manager.html
+  app.get("/sign-up", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/sign-up.html"));
+  });
 
   app.get("/", function (req, res) {
     db.Post.findAll({}).then(function (result) {
