b6fb801bbeacee3ba3a1f46c0cee46acf971b21c
Alex Tam <AJT@Alexanders-MacBook-Pro.local>
2017-07-08 21:48:24 -0700

    new db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,25 @@
 -- Create the database connactivities and specified it for use.
-CREATE DATABASE connactivities;
-USE connactivities;
+create database if not exists connactivities;
 
+use connactivities;
+
+create table if not exists Users (
+	user_id int not null auto_increment,
+    username varchar(15),
+    password varchar(15),
+    email varchar(30),
+    primary key (user_id)
+);
+
+create table if not exists Posts (
+    post_id int not null auto_increment,
+    user_id int not null,
+    description text,
+    address text,
+    time date,
+    partner_id int,
+    primary key (post_id),
+    foreign key (user_id) references Users (user_id)
+);
 
 
